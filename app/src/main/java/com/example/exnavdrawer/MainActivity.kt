b18a02d6072921d0553dc314f11089e89322f13b
Lam Nguyen <lamnt437@gmail.com>
2024-07-22 15:02:48 +0700

    attempt to add drawer menu

--- app/src/main/java/com/example/exnavdrawer/MainActivity.kt
+++ app/src/main/java/com/example/exnavdrawer/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.navigation.NavigationView
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -17,18 +18,20 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        navView.setNavigationItemSelectedListener(this)
+//        val navView: NavigationView = findViewById(R.id.nav_view)
+//        navView.setNavigationItemSelectedListener(this)
+
+        // TODO add event listener for toolbar
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.nav_new_activity -> {
-                startActivity(Intent(this, NewActivity::class.java))
-            }
-        }
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        drawerLayout.closeDrawer(GravityCompat.START)
+//        when (item.itemId) {
+//            R.id.nav_new_activity -> {
+//                startActivity(Intent(this, NewActivity::class.java))
+//            }
+//        }
+//        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+//        drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
 }
